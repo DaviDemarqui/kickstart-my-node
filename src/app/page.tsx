4d862928fbf611b75384ba6b365b3fe5d8eb44a6
DaviDemarqui <davicoding@protonmail.com>
2024-02-10 12:16:58 -0300

    More progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,58 +9,54 @@ export default function Home() {
   const [language, setLanguage] = useState('js');
   const [dependencies, setDependencies] = useState([]);
 
-
-
-
-
   return (
     <main className="flex min-h-screen items-center justify-between">
       <div className="max-md:m-0 m-20 flex">
-      <div className="w-1/2 bg-white p-5 lg:pr-28 h-screen border border-black">
-        <h1 className="text-3xl font-bold">Kickstart My Node⚡:Generate Node API Project;</h1>
-        <p className="my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. At praesentium perspiciatis non hic accusamus quibusdam provident corrupti ipsa eius facere, repellat laudantium iste porro? At qui iste voluptates necessitatibus dolor!</p>
+        <div className="w-1/2 bg-white p-5 lg:pr-28 h-screen border border-black">
+          <h1 className="text-3xl font-bold">Kickstart My Node⚡:Generate Node API Project;</h1>
+          <p className="my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. At praesentium perspiciatis non hic accusamus quibusdam provident corrupti ipsa eius facere, repellat laudantium iste porro? At qui iste voluptates necessitatibus dolor!</p>
 
-        <h2 className="text-lg font-semibold">Language</h2>
-        <div className="flex">
-          <div className="items-center mb-4 mr-2">
-            <input id="javascript-checkbox" type="checkbox" value="js" className="w-5 h-5 outline-none accent-black bg-gray-100 border-gray-300 rounded focus:ring-2" />
-            <label htmlFor="javascript-checkbox" className="ms-2 text-sm font-semibold text-gray-900">Javascript</label>
+          <h2 className="text-lg font-semibold">Language</h2>
+          <div className="flex">
+            <div className="items-center mb-4 mr-2">
+              <input id="javascript-checkbox" type="checkbox" value="js" className="w-5 h-5 outline-none accent-black bg-gray-100 border-gray-300 rounded focus:ring-2" />
+              <label htmlFor="javascript-checkbox" className="ms-2 text-sm font-semibold text-gray-900">Javascript</label>
+            </div>
+            <div className="items-center mb-4">
+              <input id="typescript-checkbox" type="checkbox" value="ts" className="w-5 h-5 outline-none accent-black bg-gray-100 border-gray-300 rounded focus:ring-2" />
+              <label htmlFor="typescript-checkbox" className="ms-2 text-sm font-semibold text-gray-900">Typescript</label>
+            </div>
           </div>
-          <div className="items-center mb-4">
-            <input id="typescript-checkbox" type="checkbox" value="ts" className="w-5 h-5 outline-none accent-black bg-gray-100 border-gray-300 rounded focus:ring-2" />
-            <label htmlFor="typescript-checkbox" className="ms-2 text-sm font-semibold text-gray-900">Typescript</label>
+
+          <h2 className="text-lg font-semibold">Project Metadata</h2>
+          <div className="block">
+            <input type="text" className="border w-full border-black mb-[3px] text-black bg-transparent py-1 px-3 outline-none" placeholder="Project Name"/>
           </div>
+          <div className="block">
+            <input type="text" className="border w-full border-black mb-[3px] text-black bg-transparent py-1 px-3 outline-none" placeholder="Version"/>
+          </div>
+          <div className="block">
+            <select className="border w-full border-black mb-[3px] text-black bg-transparent py-2 px-3 outline-none">
+              <option defaultValue="" selected disabled>Select an license</option>
+              <option defaultValue="">MIT</option>
+              <option defaultValue="">GPD</option>
+              <option defaultValue="">mm</option>
+              <option defaultValue="">NONE</option>
+            </select>
+          </div>
+          {/* <div className="block">
+            <input type="text" className="border w-full border-black mb-[3px] text-black bg-transparent py-1 px-3 outline-none" placeholder="Project Name"/>
+          </div> */}
+
+          <button className="bg-transparent border-2 flex mt-3 border-zinc-800 text-black px-3 py-2 hover:bg-black hover:text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-download" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" /><path d="M7 11l5 5l5 -5" /><path d="M12 4l0 12" /></svg>  
+            Generate
+          </button>
         </div>
 
-        <h2 className="text-lg font-semibold">Project Metadata</h2>
-        <div className="block">
-          <input type="text" className="border w-full border-black mb-[3px] text-black bg-transparent py-1 px-3 outline-none" placeholder="Project Name"/>
+        <div className="w-1/2 bg-black overflow-y-scroll border border-black p-5  h-screen">
+          <DependencySearch />
         </div>
-        <div className="block">
-          <input type="text" className="border w-full border-black mb-[3px] text-black bg-transparent py-1 px-3 outline-none" placeholder="Version"/>
-        </div>
-        <div className="block">
-          <select className="border w-full border-black mb-[3px] text-black bg-transparent py-2 px-3 outline-none">
-            <option value="" selected disabled>Select an license</option>
-            <option value="">MIT</option>
-            <option value="">GPD</option>
-            <option value="">JFIFF</option>
-            <option value="">NONE</option>
-          </select>
-        </div>
-        {/* <div className="block">
-          <input type="text" className="border w-full border-black mb-[3px] text-black bg-transparent py-1 px-3 outline-none" placeholder="Project Name"/>
-        </div> */}
-
-        <button className="bg-transparent border-2 flex mt-3 border-zinc-800 text-black px-3 py-2 hover:bg-black hover:text-white">
-          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-download" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" /><path d="M7 11l5 5l5 -5" /><path d="M12 4l0 12" /></svg>  
-          Generate
-        </button>
-      </div>
-
-      <div className="w-1/2 bg-black overflow-y-scroll border border-black p-5 lg:pr-28 h-screen">
-        <DependencySearch />
-      </div>
       </div>
     </main>
   );
--- src/components/DependencySearch.tsx
+++ src/components/DependencySearch.tsx
@@ -1,32 +1,81 @@
 import React, { useEffect, useState } from 'react'
 
+class Package { // TODO - Vai da merda
+  name: string;
+  scope: string;
+  version: string;
+  description: string;
+  date: Date;
+  links: { npm: string };
+  publisher: { username: string, email: string };
+  maintainers: { username: string, email: string }[];
+
+  constructor(data: any) {
+      this.name = data.name;
+      this.scope = data.scope;
+      this.version = data.version;
+      this.description = data.description;
+      this.date = new Date(data.date);
+      this.links = { npm: data.links.npm };
+      this.publisher = { username: data.publisher.username, email: data.publisher.email };
+      this.maintainers = data.maintainers.map((maintainer: any) => ({
+          username: maintainer.username,
+          email: maintainer.email
+      }));
+  }
+}
+
 const DependencySearch = () => {
     const [dependencies, setDependencies] = useState<any[]>([]);
-    let [search, setSearch] = useState('');
+    const [selected, setSelected] = useState<any[]>([]);
+    const [showModal, setShowModal] = useState(false);
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
         fetch(`https://registry.npmjs.org/-/v1/search?text=${search}`)
             .then((res) => res.json())
-            .then((dependency: any[]) => {
-                setDependencies(dependency)
+            .then((dependency: any) => {
+                console.log(dependency.objects);
+                setDependencies(dependency.objects as any[]);
             })
-    }, [])
+    }, [search])
+
+    const handleSelect = (pckg: Package) => {
+      setSelected(selected => [...selected, pckg]);
+      setShowModal(false);
+    }
     
   return (
     <div>
-        <h1 className="text-3xl text-white font-bold">Add dependecies here:</h1>
-        <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} className="border w-full border-white mb-[3px] text-white bg-transparent py-1 px-3 outline-none" placeholder="Search Dependency"/>
-          <div className="border-l border-white rounded-lg h-full w-full my-5">
-            <ul>
+        {/* <h1 className="text-3xl text-white font-bold">Add dependecies here:</h1> */}
+        {showModal ? (
+          <>
+          <div className="flex">
+            <input type="text" defaultValue={search} onChange={(e) => setSearch(e.target.value)} className="border w-full border-white text-white bg-transparent py-1 px-3 outline-none" placeholder="Search Dependency"/>
+            <button onClick={() => setShowModal(false)} className='ml-1 p-1 px-5 bg-transparent border border-red-500 text-red-500 hover:bg-red-500 hover:text-white'>Close</button>
+          </div>
+          <div className=" rounded-md h-full w-full">
+            <ul>              
               {dependencies.map(dep => (
-                <li key={dep}>
-                  <label className='hover:bg-white hover:text-black'>
-                    <input type="text"/> {dep}
-                  </label>
+                <li key={dep} onClick={() => handleSelect(dep)} className='py-3 hover:cursor-pointer my-[5px] border border-slate-400 hover:bg-slate-500'>
+                    <span className='mx-3 my-3 w-full text-white'>{dep.package.name}</span>
                 </li>
               ))}
             </ul>
           </div>
+          </>
+        ) : 
+          <div className="p-2 rounded-md h-full w-full my-5">
+            <button onClick={() => setShowModal(true)} className='py-2 w-full bg-transparent border border-white text-white hover:bg-white hover:text-black'>Add Dependency</button>
+            <ul>
+              {selected.map(selectedDep => (
+                <li key={"selected "+selectedDep} className='py-3 bg-slate-100 hover:cursor-pointer my-[3px]'>
+                    <span className='mx-3 my-3 w-full text-black'>{selectedDep.package.name}</span><button className='float-end mr-3 text-red-500 hover:underline px-4'>Remove</button>
+                </li>
+              ))}
+            </ul>
+          </div>
+          }
     </div>
   )
 }
